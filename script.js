adb20fc5906167dfa2266ae15e3ed5cb7bf0cd1c
Dev Jeff Alves <jeff-alves@outlook.com>
2023-04-13 21:54:00 -0300

    exercicio de aula

---
+++ script.js
@@ -0,0 +1,48 @@
+/*
+Solicitar o nome do usuário, 3 notas, calcular a média.
+Informar o nome do usuário, a média e se a média for maior que 6
+dar os parabéns, se não pedir para repetir o ano
+*/
+/*let nomeUsuario = prompt("nome de usuário");
+
+
+let Nota1 = Number(prompt("digite primeira nota"));
+let Nota2 = Number(prompt("digite segunda nota"));
+let Nota3 = Number(prompt("digite terceira nota"));
+
+let soma = (Nota1 + Nota2 + Nota3);
+let media = soma / 3;
+console.log (nomeUsuario, media, media >= 6);
+if (media >= 6) {
+    alert(nomeUsuario, ', sua média foi ', media, '. Parabéns');
+} else {
+    alert(nomeUsuario + ', sua e '+ media + "Você foi reprovado!")
+};
+*/
+
+/*solicitar um numero para o usuario
+validar se é par e informar o resultado.
+*/
+/*let num1 = prompt( "Digite o número");
+if (num1 % 2 === 0) {
+    alert('É PAR')
+} else { 
+    alert('É IMPAR')
+}
+*/
+
+/* solicitar 2 números para o usuário 
+exibir o maior 
+*/
+
+let num1 = Number(prompt ("Digite primeiro número"));
+let num2 = Number(prompt ("Digite segundo número"));
+if (num1 > num2) {
+    alert(num1)
+}
+else {
+    alert(num2)
+}
+
+
+
